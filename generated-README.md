7879ee5981d3208c519d73461ca05f7fdc71db74
William <wabarringer@gmail.com>
2023-01-21 11:34:28 -0800

    added license badges

---
+++ generated-README.md
@@ -0,0 +1,41 @@
+# a
+
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+
+## Description
+
+- a 
+- a
+- a
+- a
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+- a
+
+## Usage
+
+- a
+
+## Credits
+
+- a
+
+## License
+
+- Mozilla 
+
+## Tests
+
+- a
+
+## Questions?
+
+- You can contact me at GitHub: https://github.com/a
--- index.js
+++ index.js
@@ -1,75 +1,91 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-inquirer.prompt([
-  {
-    type: "input",
-    message: "What is your project titled?",
-    name: "title",
-  },
-  {
-    type: "input",
-    message: "What was your motivation?",
-    name: "motivation",
-  },
-  {
-    type: "input",
-    message: "Why did you build this project?",
-    name: "why",
-  },
-  {
-    type: "input",
-    message: "What problem does it solve?",
-    name: "problem",
-  },
-  {
-    type: "input",
-    message: "What did you learn?",
-    name: "learn",
-  },
-  {
-    type: "input",
-    message: "What are the steps required to install your project?",
-    name: "installation",
-  },
-  {
-    type: "input",
-    message:
-      "What are the instructions to use the project once it is installed?",
-    name: "use",
-  },
-  {
-    type: "input",
-    message:
-      "Were there any contributers, 3rd party assets or tuturials you used?",
-    name: "credits",
-  },
-  {
-    type: "checklist",
-    message: "What license do you have?",
-    choices: [
-      "Apache",
-      "Boost",
-      "BSD",
-      "Creative Commons",
-      "Eclipse",
-      "GNU",
-      "IBM",
-      "ISC",
-      "MIT",
-      "Mozilla",
-      "Open Data Commons",
-      "Perl",
-      "SIL",
-      "WTFPL",
-      "Zlib",
-    ],
-    name: "license",
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your project titled?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "What was your motivation?",
+      name: "motivation",
+    },
+    {
+      type: "input",
+      message: "Why did you build this project?",
+      name: "why",
+    },
+    {
+      type: "input",
+      message: "What problem does it solve?",
+      name: "problem",
+    },
+    {
+      type: "input",
+      message: "What did you learn?",
+      name: "learn",
+    },
+    {
+      type: "input",
+      message: "What are the steps required to install your project?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message:
+        "What are the instructions to use the project once it is installed?",
+      name: "use",
+    },
+    {
+      type: "input",
+      message:
+        "Were there any contributers, 3rd party assets or tuturials you used?",
+      name: "credits",
+    },
+    {
+      type: "checkbox",
+      message: "What license do you have?",
+      name: "license",
+      choices: ["Apache", "GNU", "IBM", "MIT", "Mozilla", "Unlicensed"],
+    },
+    {
+      type: "input",
+      message:
+        "Did you run any tests? Please provide examples on how to run them.",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "gitHub",
+    },
+  ])
+  .then((response) => {
+    const gitHubUrl = "https://github.com/" + response.gitHub;
+    if (response.license == "Apache") {
+      var licenseBadge =
+        "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    } else if (response.license == "GNU") {
+      var licenseBadge =
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    } else if (response.license == "IBM") {
+      var licenseBadge =
+        "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+    } else if (response.license == "MIT") {
+      var licenseBadge =
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else if (response.license == "Mozilla") {
+      var licenseBadge =
+        "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    } else {
+      var licenseBadge = "";
+    }
+    const mdLiteral = `# ${response.title}
 
-const htmlLiteral = `# ${response.title} 
-=============================
+${licenseBadge}
 
 ## Description
 
@@ -103,9 +119,17 @@ const htmlLiteral = `# ${response.title}
 
 ## Tests
 
--
+- ${response.tests}
 
-## Questions
+## Questions?
 
--
+- You can contact me at GitHub: ${gitHubUrl}
 `;
+    fs.writeFile("generated-README.md", mdLiteral, (err) => {
+      if (err) {
+        console.log("error");
+      } else {
+        console.log("worked");
+      }
+    });
+  });
